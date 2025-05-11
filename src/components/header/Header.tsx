d6428edeeae6e10fb17edf7e9175ef7d2e78648f
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-05-11 20:03:05 +0900

    chore: header navigation link 적용
    - header의 menu 버튼을 Link 컴포넌트 사용으로 변경하여 페이지 연결

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,19 @@
-const Header: React.FC = () => {
-  const categories = [
-    "학습하기",
-    "미니게임",
-    "신조어 등록",
-    "수어 노래",
-    "수어 번역",
-  ];
+import { Link } from "react-router-dom";
 
+const Header: React.FC = () => {
   return (
     <header className="h-[4.5rem] relative flex justify-center items-center bg-white">
       {/* TODO: logo 삽입 */}
       {/* <div className="absolute left-6">
         <img src="/logo.png" />
       </div> */}
+
       <nav className="flex space-x-8 text-gray-600">
-        {categories.map((element, idx) => (
-          <button key={idx}>{element}</button>
-        ))}
+        <Link to="/learning">학습하기</Link>
+        <Link to="/">미니게임</Link>
+        <Link to="/">신조어 등록</Link>
+        <Link to="/">수어 노래</Link>
+        <Link to="/translation">수어 번역</Link>
       </nav>
     </header>
   );
