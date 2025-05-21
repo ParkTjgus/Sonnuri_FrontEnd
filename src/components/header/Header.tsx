0a041759b45d80f38b85f78a4ccc3ba95f21a079
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-05-21 18:36:56 +0900

    chore: 헤더에 로고 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,19 +2,22 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <header className="h-[4.5rem] relative flex justify-center items-center bg-white">
-      {/* TODO: logo 삽입 */}
-      {/* <div className="absolute left-6">
-        <img src="/logo.png" />
-      </div> */}
-
-      <nav className="flex space-x-8 text-gray-600">
-        <Link to="/learning">학습하기</Link>
-        <Link to="/">미니게임</Link>
-        <Link to="/">신조어 등록</Link>
-        <Link to="/">수어 노래</Link>
-        <Link to="/translation">수어 번역</Link>
-      </nav>
+    <header className="w-full h-[4.5rem] bg-white border-b border-gray-200 px-6">
+      <div className="flex items-center justify-between h-full max-w-[1200px] mx-auto w-full">
+        <div className="flex-none mr-2">
+          <span className="font-['KoPubBatang'] text-lg whitespace-nowrap text-[#F97015]">
+            손누리
+          </span>
+        </div>
+        <nav className="flex-1 flex justify-center space-x-8 text-gray-600 whitespace-nowrap">
+          <Link to="/learning">학습하기</Link>
+          <Link to="/">미니게임</Link>
+          <Link to="/">신조어 등록</Link>
+          <Link to="/">수어 노래</Link>
+          <Link to="/translation">수어 번역</Link>
+        </nav>
+        <div className="w-[80px]" />
+      </div>
     </header>
   );
 };
