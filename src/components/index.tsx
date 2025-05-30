57844461fd369a057758f973c73cbaed00c5ce7f
ParkTjgus <129525514+ParkTjgus@users.noreply.github.com>
2025-05-30 19:11:52 +0900

    fix: component index 파일의 주소 오류 수정

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,26 +1,26 @@
-export { default as DefaultVideoBox } from './translation/DefaultVideoBox';
-export { default as VideoConnection } from './translation/VideoConnection';
-export { default as VideoRecordBox } from './translation/VideoRecordBox';
-export { default as VideoRecordControls } from './translation/VideoRecordControls';
-export { default as VideoCard } from './translation/VideoCard';
+export { default as DefaultVideoBox } from "./translation/DefaultVideoBox";
+export { default as VideoConnection } from "./translation/VideoConnection";
+export { default as VideoRecordBox } from "./translation/VideoRecordBox";
+export { default as VideoRecordControls } from "./translation/VideoRecordControls";
+export { default as VideoCard } from "./translation/VideoCard";
 
-export { default as LevelCard } from './learningLevel/LevelCard';
-export { default as LevelSection } from './learningLevel/LevelSection';
-export { default as StageSection } from './learningLevel/StageSection';
-export { default as StageCard } from './learningLevel/StageCard';
+export { default as LevelCard } from "./learningLevel/LevelCard";
+export { default as LevelSection } from "./learningLevel/LevelSection";
+export { default as StageSection } from "./learningLevel/StageSection";
+export { default as StageCard } from "./learningLevel/StageCard";
 
-export { default as IntroBanner } from './home/IntroBanner';
-export { default as FeatureCard } from './home/FeatureCard';
+export { default as IntroBanner } from "./home/IntroBanner";
+export { default as FeatureCard } from "./home/FeatureCard";
 
-export { default as Header } from './header/Header';
+export { default as Header } from "./header/Header";
 
-export { default as LearningVideo } from './learning/LearningVideo';
-export { default as AnswerButton } from './learning/AnswerButton';
-export { default as PlayButton } from './learning/PlayButton';
-export { default as SubmitButton } from './learning/SubmitButton';
-export { default as ResultBox } from './learning/ResultBox';
-export { default as PracticeButton } from './learningTest/PracticeButton';
-export { default as AnswerBox } from './learningTest/AnswerBox';
-export { default as CameraBox } from './learningTest/CameraBox';
-export { default as NextButton } from './learningTest/NextButton';
-export { default as AccuracyBox } from './learningTest/AccuracyBox';
+export { default as LearningVideo } from "./learningTest/LearningVideo";
+export { default as AnswerButton } from "./learningTest/AnswerButton";
+export { default as PlayButton } from "./learningTest/PlayButton";
+export { default as SubmitButton } from "./learningTest/SubmitButton";
+export { default as ResultBox } from "./learningTest/ResultBox";
+export { default as PracticeButton } from "./learning/PracticeButton";
+export { default as AnswerBox } from "./learning/AnswerBox";
+export { default as CameraBox } from "./learning/CameraBox";
+export { default as NextButton } from "./learning/NextButton";
+export { default as AccuracyBox } from "./learning/AccuracyBox";
